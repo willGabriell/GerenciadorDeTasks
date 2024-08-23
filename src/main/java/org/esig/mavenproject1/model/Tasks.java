010e5268ffadd8e69f87479f35211781c73744e1
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-08-23 16:00:04 -0300

    banco de dados configurado

---
+++ src/main/java/org/esig/mavenproject1/model/Tasks.java
@@ -0,0 +1,94 @@
+package org.esig.mavenproject1.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name = "tasks")
+public class Tasks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name = "title")
+    private String titulo;
+    
+    @Column(name = "description")
+    private String descricao;
+    
+    @Column(name = "responsible")
+    private String responsavel;
+    
+    @Column(name = "priority")
+    private String prioridade;
+    
+    @Column(name = "deadline")
+    @Temporal(TemporalType.DATE)
+    private Date deadline;
+    
+    @Column(name = "status")
+    private Boolean status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
